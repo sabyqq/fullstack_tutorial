1da92634b3d1695e5bef1906725eb9b173e4db52
sabyqq <61267276+sabyqq@users.noreply.github.com>
2022-08-18 14:22:59 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,8 +39,8 @@ class App extends React.Component {
 
   // ######################### click handler ###########################
   handleClick(event) {
-    const serverUrl = "http://backend-stack.apps.ap46a.prod.ole.redhat.com/keys";
-    const debugUrl = "http://backend-stack.apps.ap46a.prod.ole.redhat.com/debug";
+    const serverUrl = "http://backend:8000/keys";
+    const debugUrl = "http://backend:8000/debug";
     const buttonId = event.target.id;
     let state = this.state
     // ######################### create name ###########################
